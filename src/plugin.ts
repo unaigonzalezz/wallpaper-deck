8d213d82aa3b6b4a19fc75eb0e300d36fd0f8247
Unai González <unaiitxuu@gmail.com>
2026-05-18 17:09:34 +0200

    feat: random wallpaper action

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,10 @@
 import streamDeck from "@elgato/streamdeck";
 import { WallpaperChange } from './actions/change-wallpaper';
+import { RandomWallpaper } from './actions/random-wallpaper';
 
 streamDeck.logger.setLevel("trace");
 
 streamDeck.actions.registerAction(new WallpaperChange());
+streamDeck.actions.registerAction(new RandomWallpaper());
 
 streamDeck.connect();
